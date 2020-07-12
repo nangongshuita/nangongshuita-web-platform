d7ad072384cfd179712b3bb5cdac5c4487c97e53
Vincent Lin <60536661+vincnttt@users.noreply.github.com>
2020-07-12 21:32:25 +0800

    Update notif.js

--- nangongshuita/js/notif.js
+++ nangongshuita/js/notif.js
@@ -1,4 +1,24 @@
 function showNotif() {
-    window.location.href= '../index/login.html'
     alert("请先登录才能查看内容");
+    window.location.href= '../index/login.html'
+}
+
+function openOtherPageNotif_1() {
+    alert("您确定离开南宫水塔-留学生服务平台，打开另一个网页？");
+    window.location.href= 'https://www.jiaoyuhaizi.com/index'
+}
+
+function openOtherPageNotif_2() {
+    alert("本次活动未开始");
+    window.location.href= '../index/3_OrganizeActivity.html'
+}
+
+function openOtherPageNotif_3() {
+    alert("本次活动未开始");
+    window.location.href= '../index/3_OrganizeActivity.html'
+}
+
+function openOtherPageNotif_4() {
+    alert("本次活动未开始");
+    window.location.href= '../index/3_OrganizeActivity.html'
 }
