e3ae1e41e6433cd0f91eee7fa6be778c8e621d70
Vincent Lin <60536661+vincnttt@users.noreply.github.com>
2020-07-14 11:23:38 +0800

    Update notif.js

--- nangongshuita/js/notif.js
+++ nangongshuita/js/notif.js
@@ -3,6 +3,7 @@ function showNotif() {
     window.location.href= '../index/login.html'
 }
 
+// 留学生线下活动通用
 function openOtherPageNotif_1() {
     alert("您确定离开南宫水塔-留学生服务平台，打开另一个网页？");
     window.location.href= 'https://www.jiaoyuhaizi.com/index'
@@ -21,4 +22,35 @@ function openOtherPageNotif_3() {
 function openOtherPageNotif_4() {
     alert("本次活动未开始");
     window.location.href= '../index/3_OrganizeActivity.html'
+}
+
+// 来华奖学金申请通用
+function csc() {    // 中国政府奖学金简介
+    alert("您确定离开南宫水塔-留学生服务平台，打开另一个网页？");
+    window.location.href= 'https://www.csc.edu.cn/'
+}
+
+function gsb() {    // 中国政府奖学金-双边项目
+    alert("您确定离开南宫水塔-留学生服务平台，打开另一个网页？");
+    window.location.href= 'http://pk.chineseembassy.org/chn/zbgx/wenhuafuwu/t1722141.htm'
+}
+
+function dxjh() {   // 中国政府奖学金-中国大学计划
+    alert("您确定离开南宫水塔-留学生服务平台，打开另一个网页？");
+    window.location.href= 'http://www.moe.gov.cn/s78/A20/gjs_left/moe_850/tnull_1204.html'
+}
+
+function ccjxj() {  // 中国政府长城奖学金
+    alert("您确定离开南宫水塔-留学生服务平台，打开另一个网页？");
+    window.location.href= 'http://old.moe.gov.cn//publicfiles/business/htmlfiles/moe/moe_850/200407/1203.html'
+}
+
+function hskyx() {  // HSK优胜者奖学金
+    alert("您确定离开南宫水塔-留学生服务平台，打开另一个网页？");
+    window.location.href= 'http://www.moe.gov.cn/s78/A20/gjs_left/moe_850/tnull_1201.html'
+}
+
+function yxxs() {   // 优秀生奖学金
+    alert("您确定离开南宫水塔-留学生服务平台，打开另一个网页？");
+    window.location.href= 'https://www.chsi.com.cn/gjzxdk/zzzcjd/3/200607/20060728/553586.html'
 }
